e88ae849a131292f20cdc3485dfefb1d254a366d
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2023-03-11 17:10:34 +0200

    task 26 RemoveDuplicatesFromSortedArray done

---
+++ leetcode/problems/RemoveDuplicatesFromSortedArray26.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.problems
+{
+    internal class RemoveDuplicatesFromSortedArray26
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int index = 0;
+            for(int i=1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[index])
+                {
+                    index++;
+                    nums[index] = nums[i];
+                }
+            }
+            return ++index;
+        }
+    }
+}
