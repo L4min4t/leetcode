5c91439ea49f4627e7be2e79e356ddefdb229d21
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2023-03-11 23:12:45 +0200

    task 27 RemoveElement done

---
+++ leetcode/problems/RemoveElement27.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.problems
+{
+    internal class RemoveElement27
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int counter = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[counter] = nums[i];
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+    }
+}
