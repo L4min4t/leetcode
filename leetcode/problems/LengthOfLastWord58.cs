380b3c808d9d300aaa33b184c5b3ad06ce3af280
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2023-03-13 00:30:40 +0200

    task 58 LengthOfLastWord done

---
+++ leetcode/problems/LengthOfLastWord58.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.problems
+{
+    internal class LengthOfLastWord58
+    {
+        public int LengthOfLastWord(string s)
+        {
+            int counter = 0;
+            for(int i = s.Length - 1; i >= 0; i--)
+            {
+                if (char.IsLetter(s[i])) counter++;
+                else if (counter > 0) return counter;
+            }
+            return s.Trim().Length;
+        }
+    }
+}
