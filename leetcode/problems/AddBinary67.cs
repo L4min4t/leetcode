4f5f024df0b9827dd2cd8ff564db0f264ca721bd
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2023-03-20 17:48:14 +0200

    task 67 AddBinary done

---
+++ leetcode/problems/AddBinary67.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.problems
+{
+    internal class AddBinary67
+    {
+        public string AddBinary(string a, string b)
+        {
+            int alen = a.Length, blen = b.Length;
+            int i = 0, c = 0;
+            string ans = "";
+            while (i < alen || i < blen || c != 0)
+            {
+                int x = 0;
+                if (i < alen && a[alen - i - 1] == '1') x = 1;
+
+                int y = 0;
+                if (i < blen && b[blen - i - 1] == '1') y = 1;
+
+                int sum = x + y + c;
+                ans = sum % 2 + ans;
+                c = sum / 2;
+                i++;
+            }
+            return ans;
+        }
+    }
+}
